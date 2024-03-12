7be44d8a7bf324564bb87573c5735fa64c8debc3
Arshad Ansari <vajralaA@invenco.com>
2024-03-12 13:35:41 +0530

    added save changes dialog

--- src/components/PromptBuilder/modals/save-soft-key-modal/saveSoftKey.tsx
+++ src/components/PromptBuilder/modals/save-soft-key-modal/saveSoftKey.tsx
@@ -4,28 +4,36 @@ import SearchableDropdown from "../../../common/searchable-dropdown/searchableDr
 import { useSelector } from "react-redux";
 import { Keycode } from "../../../../models/keycode.modal";
 import { selectKeycodes } from "../../../../redux/selectors/keycodeSelectors";
-import {  useState } from "react";
+import { useState } from "react";
+import { get } from "http";
 
 interface SaveSoftKeyProps {
   hide: () => void;
   onChange: (keycode: Keycode) => void;
+  currentSoftKey: string;
 }
 
-function SaveSoftKey({ hide, onChange }: SaveSoftKeyProps) {
-  const [selectedCode, setSelectedCode] = useState<Keycode>();
-
+function SaveSoftKey({ hide, onChange, currentSoftKey }: SaveSoftKeyProps) {
   const keycodes = useSelector(selectKeycodes).map(({ code, name }) => ({
     id: code,
     code,
     name,
   }));
 
+  const getSelectedCode = () => {
+    return keycodes.find((keycode) => keycode.name === currentSoftKey);
+  };
+
+  const [selectedCode, setSelectedCode] = useState<Keycode>(
+    getSelectedCode() || ({} as Keycode)
+  );
+
   const handleSelect = (keycode: Keycode) => {
     setSelectedCode(keycode);
   };
 
   return (
-    <div>
+    <div className="save-soft-key">
       <Modal.Header>
         <h4>Soft Key Code</h4>
       </Modal.Header>
@@ -40,18 +48,28 @@ function SaveSoftKey({ hide, onChange }: SaveSoftKeyProps) {
             </>
           )}
           onSelect={handleSelect}
+          selectedCode={selectedCode?.name}
         />
       </Modal.Body>
       <Modal.Footer>
-        <button className="btn btn-link" onClick={hide}>
-          CANCEL
-        </button>
-        <button
-          className="btn btn-primary"
-          onClick={() => onChange(selectedCode || ({} as Keycode))}
-        >
-          SAVE
-        </button>
+        <div className="left-side">
+          {selectedCode.name && (
+            <button className="btn btn-gray text-danger" onClick={hide}>
+              RESET
+            </button>
+          )}
+        </div>
+        <div className="right-side">
+          <button className="btn btn-link" onClick={hide}>
+            CANCEL
+          </button>
+          <button
+            className="btn btn-primary"
+            onClick={() => onChange(selectedCode || ({} as Keycode))}
+          >
+            SAVE
+          </button>
+        </div>
       </Modal.Footer>
     </div>
   );
